1936450f081c740fe24ffff07a1a2c9be931ad9c
anzhehong <anzhehong@126.com>
2016-04-06 14:31:29 +0800

    add playing view controller

---
+++ Carols/Carols/PlayingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlayingViewController.swift
+//  Carols
+//
+//  Created by FOWAFOLO on 16/4/6.
+//  Copyright © 2016年 TAC. All rights reserved.
+//
+
+import UIKit
+
+class PlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
